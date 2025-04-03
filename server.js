95a7646a3934eb42eceab2a307facfae07c348d1
sudarshanr8660 <sudarshanhtglksfansa@gmail.com>
2025-04-03 12:17:22 +0530

    first commit

--- server.js
+++ server.js
@@ -774,9 +774,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+
