bee5e4d0d45632baa6f34354da7d815c505c80b1
sudarshanr8660 <sudarshanhtglksfansa@gmail.com>
2025-04-08 13:51:12 +0530

    first commit

--- server.js
+++ server.js
@@ -885,9 +885,9 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "build", "index.html"));
+//   });
+// }
